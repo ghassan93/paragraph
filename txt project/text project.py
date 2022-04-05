2962702ef6a20c091694e1874a2c454b5cacd14a
DESKTOP-P2PJIQU\sa <garabeed@gmail.com>
2022-04-05 16:03:27 +0300

    convert pdf to text and cut text into paragraph search keywords in paragraph and write result in output file

--- txt project/text project.py
+++ txt project/text project.py
@@ -1,37 +1,47 @@
+import os
+import pdfplumber
 
-txt = open("inputText.txt","r")
+
+# convert pdf to output file as processing.text
+processing = open("processing.txt","w")
+with pdfplumber.open('q.pdf') as pdf:
+    for page in pdf.pages:
+        numpage=page.page_number
+        txt=page.extract_text()
+        processing.write(txt)
+        print(txt)
+processing.close()  
+ 
+
+# cut text into paragraph
+txt = open("processing.txt","r")
 read_txt = txt.read()
-txt_split=read_txt.split('\n')
+txt_split=read_txt.split('\n ')
+txt.close()
 while '' in txt_split:
     txt_split.remove('')
 while ' ' in txt_split:
     txt_split.remove(' ')
+print(txt_split)    
         
-keywords=['Due date','deadline','QA date','Mandatory','Meeting','walkthrough','Intent to bid','Manual','References','Surety','bond','Maintenance','support','Timeframe','timeline','Signature','Submittal format','hard copy(ies)','Font','Page limit']
 multi_keywords = ['Original','copy','copies']
-print(txt_split)  
-# for x in txt_split:
-#     for i in keywords:
-#         if i in x : 
-#             pass
-            # (i)
-            #return x
-          
-# c='Original'            
-# for y in txt_split:
-#     for i in multi_keywords:
+
+output = open("outputText.txt","w")
+
+# search keywords in paragraph and write result in output file
+for paragraph in  txt_split:
+    checker = []
+    for word in multi_keywords:
+        checker.append(bool(paragraph.count(word)))
+    if sum(checker) > 1 :
+        output.write(paragraph+'\n')
         
-#        if  i in y:
-#          print (y)         
-           
-# txt.close()
-    
+output.close()
 
-    # with open("output.txt","+a")as outfile:  
-    #     outfile.write("\n".join(x))
+# remove processing.text
+import os
+if os.path.exists("processing.txt"):
+  os.remove("processing.txt")
+else:
+  print("The file does not exist")
   
-    #print(txt_split)  
-  
-
-
-
